34069c49e57092b62e5ddf2c2c29a4d28b0fac16
bogdan demchenko <bogdandemchenko@gmail.com>
2022-05-22 11:47:13 +0300

    1:1.3 sum of two maximums

--- sicp/chapter1/03.rkt
+++ sicp/chapter1/03.rkt
@@ -2,7 +2,26 @@
 
 (#%require rackunit)
 
-(define vasa
-   (/ 222 2))
+(define (square x)
+      (* x x))
 
-(check-equal? vasa 111)
+(check-equal? (square 3) 9)
+
+(define (sum-of-squares x y)
+      (+ (square x) (square y)))
+
+(check-equal? (sum-of-squares 2 3) 13)
+
+(define (max x y) (if (> x y) x y))
+
+(check-equal? (max 4 5) 5)
+(check-equal? (max 6 2) 6)
+
+(define (sum-two-maximum-numbers a b c)
+      (cond ((and (< a b) (< a c)) (sum-of-squares b c))
+            ((and (< b a) (< b c)) (sum-of-squares a c))
+            (else (sum-of-squares a b))))
+
+(check-equal? (sum-two-maximum-numbers 4 3 1) 25)
+(check-equal? (sum-two-maximum-numbers 4 1 3) 25)
+(check-equal? (sum-two-maximum-numbers 1 3 4) 25)
